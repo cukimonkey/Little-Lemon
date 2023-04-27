9e4df16fc1d115753f92c465c330012fa4adac61
Ursula Armstrong <cukimonkey@gmail.com>
2023-04-27 19:53:35 +0100

    Added metatags to the Header

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,10 @@ import React from "react";
 function Header(){
 return (
     <header>
-        {/* Header content goes here */}
+        <meta charSet="UTF-8"/>
+        <meta name="og:title" content="Little Lemon Restaurant"/>
+        <meta name="og:description" content="We are a family owned Mediterreanian Restaurant, focused on traditional recipes served with a modern twist."></meta>
+        <meta name="og:image" content={process.env.PUBLIC_URL + "/restaurantfood.jpg"}/>
     </header>
 );
 }
