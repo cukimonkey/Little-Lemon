77f8d82769425473e46d10759d6e743fee6d8452
Ursula Armstrong <cukimonkey@gmail.com>
2023-04-27 19:19:21 +0100

    Adding footer elements

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,40 @@ import React from "react";
 
 function Footer(){
 return (
-    <footer>
-       {/* Footer content goes here */}
+    <footer className='footer'
+    style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%"
+    }}>
+      <div className="footerelement">
+            <img className="footerlogo" height='100px' src={process.env.PUBLIC_URL + "/Footer logo.png"} alt="logo"/>
+      </div>
+      <div className="footerelement">
+        <ul className="footerlist">
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservation</li>
+            <li>Order Online</li>
+            <li>Login</li>
+        </ul>
+        </div>
+      <div className="footerelement">
+        <ul className="footerlist">
+            <li>Address</li>
+            <li>Phone number</li>
+            <li>Email</li>
+        </ul>
+      </div>
+      <div className="footerelement">
+      <ul className="footerlist">
+            <li>Facebook</li>
+            <li>Instagram</li>
+            <li>Twitter</li>
+        </ul>
+        </div>
     </footer>
 );
 }
