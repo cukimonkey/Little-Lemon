3dde3421f7f7fa18f704a2b5e35cb3d29f8ee555
Ursula Armstrong <cukimonkey@gmail.com>
2023-04-28 19:18:00 +0100

    Applied style element to the nav bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,28 +1,22 @@
-import { flexbox } from "@chakra-ui/react";
 import React from "react";
+import "./App.css";
+
 
 function Nav(){
 return (
     <nav className="navbar">
-        <ul className="navelement"
-            style={{
-                listStyleType: "none",
-                padding: 0,
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "100%"
-            }}
-          >
-            <img src={process.env.PUBLIC_URL + "/Main logo.png"} alt="logo"/>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/menu">Menu</a></li>
-            <li><a href="/reservation">Reservation</a></li>
-            <li><a href="/order-online">Order Online</a></li>
-            <li><a href="/login">Login</a></li>
+        <div className="navLogo">
+            <img src={process.env.PUBLIC_URL + "/Main logo.png"} height='50px' alt="logo"/>
+        </div>
+        <ul className="navElements">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/menu">Menu</a></li>
+        <li><a href="/reservation">Reservation</a></li>
+        <li><a  href="/order-online">Order Online</a></li>
+        <li><a href="/login">Login</a></li>
         </ul>
-    </nav>
+     </nav>
 );
 }
 
